03b0375eef0ab721c9625b9de7a5f5fe287a3d57
axyres <prodesaxy@gmail.com>
2023-09-18 22:49:43 +0700

    Add Marco script AHK

--- bin/runScriptAHK.bat
+++ bin/runScriptAHK.bat
@@ -1,6 +1,41 @@
-set AHKScriptPath=..\script\Duplicate_Control.ahk
+@echo off
+set PressControlAHK=..\script\Duplicate_Control.ahk
+set Marco6Nong=..\script\Marco_6_nong.shk
 
-start "" "%USERPROFILE%\Desktop\AutoHotkey\AutoHotkeyU64.exe" "%AHKScriptPath%"
+:menu
+cls
+echo Options:
+echo 1. Increase Press Control
+echo 2. Macro 6 Nong Vip
+echo 3. Add all funtions
+echo 4. Exit
+
+set /p choice=Enter your choice (1/2/3/4): 
+
+start "" "%USERPROFILE%\Desktop\AutoHotkey\AutoHotkeyU64.exe" "%PressControlAHK%"
+
+if "%choice%"=="1" (
+    start "" "%USERPROFILE%\Desktop\AutoHotkey\AutoHotkeyU64.exe" "%PressControlAHK%"
+)
+
+if "%choice%"=="2" (
+    start "" "%USERPROFILE%\Desktop\AutoHotkey\AutoHotkeyU64.exe" "%Marco6Nong%"
+)
+
+if "%choice%"=="3" (
+    for %%f in (script\*.ahk) do (
+        start "" "%USERPROFILE%\Desktop\AutoHotkey\AutoHotkeyU64.exe" "%%f"
+    )
+)
+
+if "%choice%"=="4" (
+    goto end
+)
+
+echo Invalid choice. Please try again.
+pause
+goto menu
+
 if %errorlevel%==0 (
     echo Running script done. Kindly access to url: https://keytest.vn to test
 ) else (
---
+++ script/Marco_6_nong.ahk
@@ -0,0 +1,14 @@
+; Remap the forward mouse button to perform three left clicks with a sleep between each click
+XButton2::
+    Click Down  ;
+    Sleep 25  ;
+    Click Up  ;
+    Sleep 25  ;
+    Click Down  ;
+    Sleep 25  ;
+    Click Up  ;
+    Sleep 25  ;
+    Click Down  ;
+    Sleep 25  ;
+    Click Up  ;
+return
