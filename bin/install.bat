e3ac56ac3941a1c6035dea86616bee6b7e7ec863
axyres <prodesaxy@gmail.com>
2023-09-18 13:35:07 +0700

    Update Script Install

--- bin/install.bat
+++ bin/install.bat
@@ -1,6 +1,7 @@
 @echo off
 set InstallerURL=https://www.autohotkey.com/download/2.0/AutoHotkey.zip
 set AHKScriptPath=..\script\Duplicate_Control.ahk
+set SevenZipPath="C:\Program Files\7-Zip\7z.exe"
 
 echo Checking internet connection...
 
@@ -14,10 +15,28 @@ if %errorlevel%==0 (
     exit /b 1
 )
 
+echo Checking Tool Requiredment...
+
+if exist %SevenZipPath% (
+    echo All tool is already. Proceeding with installation...
+) else (
+    echo Some missing tool. Downloading and installing...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.7-zip.org/a/7z1900-x64.exe', '7z.exe')"
+    start /wait 7z.exe /S   REM Install 7-Zip silently
+    del 7z.exe
+)
+
 echo Installing AutoHotKey...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%InstallerURL%', 'AutoHotkey.zip')"
-powershell -Command "Expand-Archive -Path .\AutoHotkey.zip -DestinationPath .\AutoHotkey"
-echo Installing Done.
+%SevenZipPath% x AutoHotkey.zip -oAutoHotkey
+
+if %errorlevel%==0 (
+    echo Installation Done.
+) else (
+    echo Installation failed.
+    echo Exiting the script.
+    exit /b 1
+)
 
 timeout /t 10 /nobreak > nul
 
