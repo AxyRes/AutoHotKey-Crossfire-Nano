0009f6b6bf0e55fdac193bfa5e6903017f2d9268
axyres <prodesaxy@gmail.com>
2023-09-18 23:33:29 +0700

    Add exit in option

--- bin/runScriptAHK.bat
+++ bin/runScriptAHK.bat
@@ -9,6 +9,8 @@
     powershell -command "Start-Process '%0' -Verb RunAs"
     exit /b 0
 )
+
+:runscript
 set PressControlAHK=Duplicate_Control.ahk
 set Marco6Nong=Marco_6_nong.ahk
 set "DestinationFolder=%USERPROFILE%\Desktop\AutoHotkey\script"
@@ -17,24 +19,34 @@ set "DestinationFolder=%USERPROFILE%\Desktop\AutoHotkey\script"
 cls
 echo Options:
 echo 1. Increase Press Control
-echo 2. Macro 6 Nong Vip (Beta)
-echo 3. Add all funtions
+echo 2. Macro 6 Nong Vip
+echo 3. Add all functions
 echo 4. Exit
 
 set /p choice=Enter your choice (1/2/3/4): 
 
 if "%choice%"=="1" (
     start "" "%USERPROFILE%\Desktop\AutoHotkey\AutoHotkeyU64.exe" "%DestinationFolder%\%PressControlAHK%"
+    echo Increase Control have been done.
+    timeout /t 10 /nobreak > nul
+    goto end
 )
 
 if "%choice%"=="2" (
     start "" "%USERPROFILE%\Desktop\AutoHotkey\AutoHotkeyU64.exe" "%DestinationFolder%\%Marco6Nong%"
+    echo Macro 6 Nong have been done.
+    timeout /t 10 /nobreak > nul
+    goto end
 )
 
 if "%choice%"=="3" (
     for %%f in (%DestinationFolder%\*.ahk) do (
+        echo Install %%f
         start "" "%USERPROFILE%\Desktop\AutoHotkey\AutoHotkeyU64.exe" "%%f"
     )
+    echo All tool have been install.
+    timeout /t 10 /nobreak > nul
+    goto end
 )
 
 if "%choice%"=="4" (
@@ -45,10 +57,11 @@ echo Invalid choice. Please try again.
 pause
 goto menu
 
+:end
 if %errorlevel%==0 (
-    echo Running script done. Kindly access to url: https://keytest.vn to test
+    echo Running script done. Kindly access to URL: https://keytest.vn to test
 ) else (
-    echo Have something error to running. PLease check again script AHK and run script runScriptAHK.bat again.
+    echo There was an error while running the script. Please check your AHK scripts and run the 'runScriptAHK.bat' again.
     echo Exiting the script.
     pause
     exit /b 1
